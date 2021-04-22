05e8f193be4305128ef7bcd586cb06ba6f75cbab
nickolanack <>
2021-04-22 15:00:05 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.UserWizardBuilder.20.1.content.1.script.js
+++ app/widget.UserWizardBuilder.20.1.content.1.script.js
@@ -2,7 +2,7 @@ var button=new Element('button', {
 
 					html: 'Create account',
 					style: "background-color:#EDC84C;",
-					"class": "primary-btn"
+					"class": "primary-btn btn-other"
 
 				});
 				
