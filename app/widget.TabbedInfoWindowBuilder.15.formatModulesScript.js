2f7a0836330cee0ac8276232767d790da225e3fa
nickolanack <>
2021-04-22 18:10:43 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.TabbedInfoWindowBuilder.15.formatModulesScript.js
+++ app/widget.TabbedInfoWindowBuilder.15.formatModulesScript.js
@@ -4,7 +4,7 @@ if(AppClient.getUserType()!=="admin"){
 }else{
     
     list.tab.push(new Element('button',{html:"Edit", events:{click:function(){
-        console.error('todo: detect tab '+(tab._currentTab||0));
+        console.error('todo: detect tab '+(application.getNamedValue('currentTabPage'));
         (new UIModalDialog(application, item, {	"formName":"MapItemTemplate", "formOptions":{"-template":"form"}})).show();
         
     }}}));    
