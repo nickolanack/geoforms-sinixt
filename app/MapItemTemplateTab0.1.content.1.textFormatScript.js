ae4e8a57c3575c44b8b245f51ca641b820cff0f6
nickolanack <>
2021-04-22 18:54:53 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/MapItemTemplateTab0.1.content.1.textFormatScript.js
+++ app/MapItemTemplateTab0.1.content.1.textFormatScript.js
@@ -1,10 +1,24 @@
 //return text.split("\n\n").shift().split('Layer Keywords:').shift();
 
+
+var initialText=false
+
 this.addInputFilter(function(text){
-    console.error('input')
-    return text;
+    console.error('input filter')
+    
+    if(initialText===false){
+        initialText=text;
+    }
+    
+    return text.split("\n\n").shift().split('Layer Keywords:').shift();
 });
 this.addOutputFilter(function(text){
-    console.error('output')
-    return text;
+    console.error('input filter');
+    var chunks=initialText.split("\n\n");
+    var section=chunks.shift().split('Layer Keywords:')
+    section[0]=text;
+    chunks[0]=section.join('Layer Keywords:');
+    var formatted=chunks.join("\n\n");
+    return initialText;
+    
 });
