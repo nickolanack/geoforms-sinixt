4f91b51a9baeee9f7ad51197c9e3858b7dbe804c
nickolanack <>
2021-04-22 21:30:25 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userAccessForm.1.content.4.onRenderTextFieldScript.js
+++ app/userAccessForm.1.content.4.onRenderTextFieldScript.js
@@ -1,21 +1,49 @@
 var div=new Element('container',{"class":"btn-group-access"});
 
 
+var label=new Element('label');
+
+div.appendChild(label);
+
+var toTime=function(t){
+    
+    label.innerHTML=t+" day"+(t==1?"":"s")+". ";
+    label.addClass('valid');
+    
+    return Math.floor(((new Date()).getTime()+1000*3600*24*t)/1000);
+    
+}
+
 div.appendChild(new Element('button',{
-    html:"1 day"
+    html:"1 day",
+    events:{click:function(){
+        textField.setValue(toTime(1))
+    }}
 }))
 
 div.appendChild(new Element('button',{
-    html:"2 days"
+    html:"2 days",
+    events:{click:function(){
+        textField.setValue(toTime(2))
+    }}
 }))
 
 div.appendChild(new Element('button',{
-    html:"1 week"
+    html:"1 week",
+    events:{click:function(){
+        textField.setValue(toTime(7))
+    }}
 }))
 
 div.appendChild(new Element('button',{
-    html:"Custom"
+    html:"Custom",
+    events:{
+        click:function(){
+            wizard.displayStep(3)
+        }
+    }
 }))
+
 
 
 textField.getElement().appendChild(div);
