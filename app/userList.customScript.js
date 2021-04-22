a5702695d8780741f35d2c8727bca1a68bac0030
nickolanack <>
2021-04-22 16:04:48 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userList.customScript.js
+++ app/userList.customScript.js
@@ -2,4 +2,8 @@ if(!GetClient()->isAdmin()){
     return array();
 }
 
-return array("results"=>GetClient()->listUsers());
+return array("results"=>array_map(function($u){
+    
+    return $u;
+    
+},GetClient()->listUsers()));
