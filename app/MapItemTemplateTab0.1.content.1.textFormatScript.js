a1c61b6ac7a858ff0cfd26123280ec47df655ae9
nickolanack <>
2021-04-22 18:40:58 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/MapItemTemplateTab0.1.content.1.textFormatScript.js
+++ app/MapItemTemplateTab0.1.content.1.textFormatScript.js
@@ -1,1 +1,10 @@
-return text.split("\n\n").shift().split('Layer Keywords:').shift();
+//return text.split("\n\n").shift().split('Layer Keywords:').shift();
+
+object._module.getTextField().addInputFilter(function(text){
+    console.error('input')
+    return text;
+});
+object._module.getTextField().addOutputFilter(function(text){
+    console.error('output')
+    return text;
+});
