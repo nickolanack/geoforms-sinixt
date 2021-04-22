7c3deb87601ed2a4bed661cf078c9eb8b0c6a689
nickolanack <>
2021-04-22 16:06:52 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userList.customScript.js
+++ app/userList.customScript.js
@@ -1,8 +1,16 @@
 if(!GetClient()->isAdmin()){
     return array();
 }
-
+GetPlugin('Attributes');
 return array("results"=>array_map(function($u){
+    
+    
+    
+		
+		
+	$access=(new \attributes\Record('userTimedAccess'))->getValues($u['id'], 'user'),
+    
+    $u['access']=$access;
     
     return $u;
     
