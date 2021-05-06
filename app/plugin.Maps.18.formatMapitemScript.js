89a168320b5969bfa80e852165e31fc75f9e1071
nickolanack <>
2021-05-06 20:01:27 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.18.formatMapitemScript.js
+++ app/plugin.Maps.18.formatMapitemScript.js
@@ -5,7 +5,7 @@ if($feature->layerId!=17){
    $layer = (new \spatial\LayerLoader())->fromId($feature->layerId);
    $parseSettings=$layer->getParseSettings();
    if(key_exists('use-icon', $parseSettings)){
-       $feature->marker->style=$parseSettings->{'use-icon'}
+       $feature->marker->style=$parseSettings->{'use-icon'};
    }
     
 }
