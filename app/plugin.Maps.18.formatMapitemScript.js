1d63a7edf9a17dec4d914ec5ea3079b010669e7e
nickolanack <>
2021-05-06 20:00:51 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.18.formatMapitemScript.js
+++ app/plugin.Maps.18.formatMapitemScript.js
@@ -1,4 +1,15 @@
-$feature->layerId=17;
+
 unset($feature->marker->style);
+if($feature->layerId!=17){
+    
+   $layer = (new \spatial\LayerLoader())->fromId($feature->layerId);
+   $parseSettings=$layer->getParseSettings();
+   if(key_exists('use-icon', $parseSettings)){
+       $feature->marker->style=$parseSettings->{'use-icon'}
+   }
+    
+}
+
+$feature->layerId=17;
 error_log(print_r($feature, true));
 return $feature;
