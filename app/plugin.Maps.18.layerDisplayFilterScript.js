13592a123d2263093bb7d32662a2312b02d0e0df
nickolanack <>
2021-05-20 16:36:34 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.18.layerDisplayFilterScript.js
+++ app/plugin.Maps.18.layerDisplayFilterScript.js
@@ -6,7 +6,19 @@ if($layer->getId()!==17){
    //error_log(print_r($feature['icon'],true));
    $parseSettings=$layer->getParseSettings();
    if(key_exists('filter-icon-substr', $parseSettings)){
-       return key_exists('icon', $feature)&&strpos($feature['icon'], $parseSettings->{'filter-icon-substr'})!==false;
+       
+       $filter = $parseSettings->{'filter-icon-substr'};
+       if(is_array($filter)){
+           
+           foreach($filter as $filterStr){
+               if(key_exists('icon', $feature)&&strpos($feature['icon'],$filterStr)!==false){
+                   return true;
+               }
+           }
+           return false;
+       }
+       
+       return key_exists('icon', $feature)&&strpos($feature['icon'],$filter)!==false;
    }
    
    if(key_exists('filter-type', $parseSettings)){
