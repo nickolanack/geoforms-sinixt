6e258a13b3e17387105468cc7b34d7beb84a0889
nickolanack <>
2021-05-20 18:54:47 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userPanel.content.1.getItemListScript.js
+++ app/userPanel.content.1.getItemListScript.js
@@ -2,6 +2,9 @@
 		'widget': "userList",
 
 	})).addEvent('success',function(response){
+	       
+	   application.setNamedValue('userList', response.results);
+	    
 	    callback(response.results.map(function(u){
 	        return new MockDataTypeItem(Object.append(u, {type:"user"}));
 	    }));
