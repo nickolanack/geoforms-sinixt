84f241151f930a880de67860b7bcaac8053b9477
nickolanack <>
2021-07-02 23:01:27 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/dailyWord.customScript.js
+++ app/dailyWord.customScript.js
@@ -4,7 +4,7 @@ $list=GetWidget('dictionary')->getParameter('dictionary');
 if(empty($list)){
     $list=array((object) array('name'=>'add some words', 'english'=>'', 'description'=>''));
 }
-array_shuffle($list);
+shuffle($list);
 return array(
         'word'=>array_shift($list)
     );
