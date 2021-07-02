2d8b4ae7d6bef0252ded29ee387a196715a74538
nickolanack <>
2021-07-02 22:50:31 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/wordListItem.title.1.script.js
@@ -0,0 +1,79 @@
+
+var button=new Element('button', {
+
+					html: 'Edit',
+					style: "background-color:#EDC84C;",
+					"class": "primary-btn add-document edit-document"
+
+				});
+				
+   
+        
+        
+            
+        
+        
+			
+
+
+ (new UIModalFormButton(button, application, item, {
+
+					formOptions: {
+						_template: "form"
+					},
+					formName: "documentForm",
+
+				})).addEvent('complete', function() {
+
+				    
+				    
+				    (new AjaxControlQuery(CoreAjaxUrlRoot, "get_configuration_field", {
+                    		'widget': "documents",
+                    		'field': "documents"
+                    	})).addEvent('success',function(response){
+                    	    
+                    	    
+                    	    var values=response.value;
+                    	    if(!(values&&values.map)){
+                    	        return;
+                    	    }
+                    
+                             var files=[];
+                             values.forEach(function(v, i){
+                                 
+                                 if(i==item._getId()){
+                                     files.push( {
+                            			    name:item.getName(),
+                            			    description:item.getDescription()
+                            			});
+                            			return;
+                                 }
+                                 
+                                 return files.push(v);
+                                 
+                                 
+                             });
+                    
+                    
+                        (new AjaxControlQuery(CoreAjaxUrlRoot, "set_configuration_field", {
+                    		'widget': "documents",
+                    		'field': {
+                    			"name":"documents",
+                    			"value":files
+                    		}
+                    	})).addEvent('success',function(response){
+                    
+                    	}).execute();
+                    
+                    
+                    }).execute();
+				    
+				    
+				    
+				    
+				    
+				    
+
+				});
+				
+				return button;
