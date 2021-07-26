564c4d72e4c7e4e79843cf90b943180b3376e7e4
nickolanack <>
2021-07-26 18:54:53 +0000

    LB -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.18.layerDisplayChildLayersScript.js
+++ app/plugin.Maps.18.layerDisplayChildLayersScript.js
@@ -1,3 +1,3 @@
-if($layer->getId()!==17/*&&$layer->getId()!==23*/&&$layer->getId()!==25){
+if($layer->getId()!==17&&$layer->getId()!==26/*&&$layer->getId()!==23*/&&$layer->getId()!==25){
     return array(17);
 }
