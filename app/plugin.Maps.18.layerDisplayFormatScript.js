956cf291378691eed0e7db17504195406c165d81
nickolanack <>
2021-07-26 18:54:21 +0000

    LB -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.18.layerDisplayFormatScript.js
+++ app/plugin.Maps.18.layerDisplayFormatScript.js
@@ -1,6 +1,9 @@
 if($feature['type']==='marker'){
-    
+    try{
     $feature['icon']=UrlFrom($layer->getIcon().'?thumb=>48x>48');
+    }catch(\Exception $e){
+        error_log($e->getMessage());
+    }
 }
 
 return $feature;
