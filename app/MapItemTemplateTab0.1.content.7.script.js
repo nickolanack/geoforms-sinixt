7cb714d4ad55fad6c1179926dedba1aebe06d570
nickolanack <>
2021-09-16 18:27:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/MapItemTemplateTab0.1.content.7.script.js
+++ app/MapItemTemplateTab0.1.content.7.script.js
@@ -1,11 +1,13 @@
 var button=new Element('button', {
 
 					html: 'Delete',
-					style: "background-color:#EDC84C;",
+					style: "background-color: crimson;color: white;margin-left: 0;",
 					"class": "primary-btn add-document",
 					events:{click:function(){
 					    
-					       wizard.cancel(); 
+					    if(confirm("Are you sure you want to delete this marker?")){					       
+					        wizard.cancel(); 
+					    }
 					    
 					}}
 
