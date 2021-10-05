a218ed7c28cd44e55f5387785f8028d0580c8454
nickolanack <>
2021-10-05 13:48:33 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/dailyWord.customScript.js
+++ app/dailyWord.customScript.js
@@ -4,7 +4,29 @@ $list=GetWidget('dictionary')->getParameter('dictionary');
 if(empty($list)){
     $list=array((object) array('name'=>'add some words', 'english'=>'', 'description'=>''));
 }
-shuffle($list);
+
+
+//https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+$fisherYatesShuffle=function(&$items, $seed)
+{
+    @mt_srand($seed);
+    for ($i = count($items) - 1; $i > 0; $i--)
+    {
+        $j = @mt_rand(0, $i);
+        $tmp = $items[$i];
+        $items[$i] = $items[$j];
+        $items[$j] = $tmp;
+    }
+};
+
+//shuffle($list);
+
+$fisherYatesShuffle($list, 0);
+
+$day=intval(date('z'))%count($list);
+
+
+
 return array(
-        'word'=>array_shift($list)
+        'word'=>$list[$day];
     );
