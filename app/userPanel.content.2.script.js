8ea96d31df88ac31ea2d36f07232b86055d9ae1a
nickolanack <>
2022-01-17 18:28:22 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/userPanel.content.2.script.js
@@ -0,0 +1,41 @@
+if(AppClient.getUserType()!="admin"){
+    return;
+}
+
+var item=new MockDataTypeItem({
+    id:-1,
+    name:"New Document",
+    description:"",
+    mutable:true
+    
+});
+
+
+var button=new Element('button', {
+
+					html: 'Invite User',
+					//style: "background-color:#EDC84C;",
+					"class": "primary-btn add-user"
+
+				});
+
+ (new UIModalFormButton(button, application, item, {
+
+					formOptions: {
+						_template: "form"
+					},
+					formName: "inviteUserForm",
+
+				})).addEvent('complete', function() {
+
+				    
+				    
+				  
+                    
+                    }).execute();
+				    
+				    
+
+				});
+				
+				return button;
