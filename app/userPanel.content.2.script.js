8332bb32342d663fc6773c2ec0d9678f5df0b8ad
nickolanack <>
2022-01-17 21:54:24 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userPanel.content.2.script.js
+++ app/userPanel.content.2.script.js
@@ -2,13 +2,48 @@ if(AppClient.getUserType()!="admin"){
     return;
 }
 
-var item=new MockDataTypeItem({
-    id:-1,
-    name:"New Document",
-    description:"",
-    mutable:true
-    
-});
+
+
+var userInvite=(new MockDataTypeItem({
+            mutable: true,
+            name: '',
+            email:''
+        }));
+
+
+var InviteRequest=new Class_({
+            Extends:AjaxControlQuery,
+            initialize:function(credentials){
+                this.parent(FrameworkCMSAjaxUrlRoot, "invite", credentials);
+            }
+        });
+
+        userInvite.addEvent('save',function(){
+
+        	(new InviteRequest({
+                	name:userInvite.getName(),
+                	email:userInvite.getEmail(),
+                	'plugin':"Users"
+                }).addEvent("onSuccess",function(response){
+                        
+                	if(response.error){
+                        NotificationBubble.Make(response.error);
+                    }
+
+                        if(response.success&&response.subscription){
+
+                            var container=new Element("div", {"class":"progress"});
+
+                            (new UITaskProgressSubscription(container, response)).addEvent("complete",function(){
+                            	
+                            })
+                            return;
+                        }
+
+                    })).execute();
+
+
+        });
 
 
 var button=new Element('button', {
@@ -19,7 +54,7 @@ var button=new Element('button', {
 
 				});
 
- (new UIModalFormButton(button, application, item, {
+ (new UIModalFormButton(button, application, userInvite, {
 
 					formOptions: {
 						_template: "form"
