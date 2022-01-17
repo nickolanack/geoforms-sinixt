2a4dadf184317c1388a19ff2ae71534de1852b6e
nickolanack <>
2022-01-17 17:59:04 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/userAccessForm.1.content.5.script.js
@@ -0,0 +1,13 @@
+var removeUser= (new Element('button',{
+        html:"Delete User",
+        "events":{
+            click:function(){
+                
+               
+            }  
+        },
+        "class":"primary-btn form-btn"
+    }));
+    
+    
+return removeUser;
