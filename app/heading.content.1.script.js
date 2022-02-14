4b11e784c3f4ea293edf67bac5060232b67baace
nickolanack <>
2022-02-14 18:53:31 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/heading.content.1.script.js
+++ app/heading.content.1.script.js
@@ -1,6 +1,6 @@
 var title='Sinixt';
 var sub ='<p>'+`Sinixt are Interior Salishan Peoples whose təmxʷúlaʔxʷ (traditional territory) lies in the Columbia Mountains. 
-Their Sn-səlxcin Dialect reflects the globally unique Inland Temperate Rainforest of their homeland. 
+Their Snsəlxcin Dialect reflects the globally unique Inland Temperate Rainforest of their homeland. 
 Declared extinct for the purposes of the Indian Act by the Canadian government in 1956, 
 Sinixt continue their work of resurgence from bureaucratic genocide through initiatives like this site, 
 which offers information on their millennia-long and ongoing relationship to the land.`+'</p>';
